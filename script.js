187d53981fee47092b3857f89292f666e30efb70
tul0n <sayeeda.saima@selise.ch>
2020-03-16 13:20:17 +0600

    new

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 
 var numbers = [1,2,3];
-
-function dispaly(){
 document.getElementById("area").innerHTML = numbers;
+
+function display(){
+  document.getElementById("area").innerHTML = numbers;
 }
 
-function update(){
-      
-    dispaly();
-}
-function enterFirst(){
+
+
+    document.getElementById("enterLast").addEventListener("click", function(){
+         var x= document.getElementById("newNumber");
+         var y= x.value;
+         numbers.push(y);
+         display();
+    }
+    )
+
+    document.getElementById("enterFirst").addEventListener("click", function(){
+        var x= document.getElementById("newNumber");
+        var y= x.value;
+        numbers.unshift(y);
+        display();
+   }
+   )
+
+    document.getElementById("deleteLast").addEventListener("click", function(){
+    var x= document.getElementById("newNumber");
+    var y= x.value;
+    numbers.pop(y);
+    display();
+    }
+    )
     
-    dispaly();
-}
-function enterLast(){
     
-    dispaly();
-}
-function deleteFirst(){
-    dispaly();
-}
-function deleteLast(){
-    dispaly();
-}
+    document.getElementById("deleteFirst").addEventListener("click", function(){
+    var x= document.getElementById("newNumber");
+    var y= x.value;
+    numbers.shift(y);
+    display();
+    }
+    )
+
+
+    document.getElementById("sort").addEventListener("click", function(){
+        numbers.sort();
+        display();
+    }
+    )
+    
