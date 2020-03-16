4614c8fd17c735a10e2a9d87d44c21692400afdb
tul0n <sayeeda.saima@selise.ch>
2020-03-16 13:11:15 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var numbers = [1,2,3];
+
+function dispaly(){
+document.getElementById("area").innerHTML = numbers;
+}
+
+function update(){
+      
+    dispaly();
+}
+function enterFirst(){
+    
+    dispaly();
+}
+function enterLast(){
+    
+    dispaly();
+}
+function deleteFirst(){
+    dispaly();
+}
+function deleteLast(){
+    dispaly();
+}
